e82315d9878ac4c2caff52b1c45a20588fd89310
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-24 23:28:50 -0400

    transfered from sensei api

---
+++ src/main/java/com/adhocsensei/ahscourseservice/AhsCourseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahscourseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsCourseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsCourseServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/adhocsensei/ahscourseservice/dao/CourseRepository.java
@@ -0,0 +1,16 @@
+package com.adhocsensei.ahscourseservice.dao;
+
+import com.adhocsensei.ahscourseservice.dto.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByTitle(String title);
+    List<Course> findByCategory(String category);
+    List<Course> findByLocation(String location);
+    List<Course> findByDate(String date);
+//    find created courses by user
+}
